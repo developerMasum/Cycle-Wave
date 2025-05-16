0ae2fb70a1e8748501dbce610142d05c4dc10daf
developerMasum <developer.masum2001@gmail.com>
2025-05-16 23:22:00 +0600

    added redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/router.tsx";
-import { ThemeProvider } from "./providers/theme-provider.tsx"
+import { ThemeProvider } from "./providers/theme-provider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
+      <Provider store={store}>
         <RouterProvider router={router} />
-    
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 );
---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { TUser } from "../../../types";
+import type { RootState } from "../../store";
+
+type TAuthState = {
+  user: null | TUser;
+  token: null | string;
+};
+
+const initialState: TAuthState = {
+  user: null,
+  token: null,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const selectCurrentUser = (state: RootState) => state.auth.user;
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
